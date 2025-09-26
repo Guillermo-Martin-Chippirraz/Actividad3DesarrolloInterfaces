2447e2ef111d0dbb3a8a31801846ecb1c3ef459c
gmartin443 <guillermomartinchippirraz@gmail.com>
2025-09-26 16:33:00 +0200

    v1.0 Completo

---
+++ src/main/java/com/mycompany/actividad3/Actividad3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividad3;
+
+/**
+ *
+ * @author tarde
+ */
+public class Actividad3 {
+
+    public static void main(String[] args) {
+        /*
+        LoginDessigned login = new LoginDessigned();
+        login.setVisible(true);
+        */
+        
+        LoginProgrammed login = new LoginProgrammed();
+        login.setVisible(true);
+    }
+}
